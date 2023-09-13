ba43b141bf8c487f0293d8f3944bf7caa2ec2c29
Zane Camp <zmcamp1306@muleriders.saumag.edu>
2023-09-13 17:09:29 -0500

    Added onto Inventory Script

--- Assets/Testing/ZaneTesting/InventoryScripts/InventorySystem.cs
+++ Assets/Testing/ZaneTesting/InventoryScripts/InventorySystem.cs
@@ -44,16 +44,47 @@ public class InventorySystem : MonoBehaviour
         {
             value.RemoveFromStack();
 
-            if(value.stackSize == 0)
+            if (value.stackSize == 0)
             {
                 inventory.Remove(value);
                 m_itemDictionary.Remove(referenceData);
             }
         }
     }
+
+  /*  void Update()
+    {
+        if (!useLookAt)
+        {
+            _targetPosition = _parent.position + _parent.forward = 2f + new Vector3(0f, 2f, 0f);
+        }
+
+        _lookAtTarget.transform.position = Vector3.Lerp(_lookAtTarget.transform.position, _targetPosition, Time.deltaTime * lookSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_lookAtTarget != null && _lookAtTarget.TryGetComponent<ItemObject>(out ItemObject item))
+            {
+                item.OnHandlePickupItem();
+                itemController.SetTargetPosition(item.transform);
+                itemController.Pickup();
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.GetComponent<PointOfInterest>())
+        {
+            _targetPosition = collider.transform.position;
+            useLookAt = true;
+        }
+    }
+    public void OnTriggerExit(Collider collider)
+    {
+        useLookAt = false;
+    } */
 }
-
-
 [Serializable]
 public class InventoryItem
 {
@@ -77,13 +108,13 @@ public class InventoryItem
     }
 }
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour //handles the pickup for each item
 {
     public InventoryItem referenceItem;
 
     public void OnHandlePickupItem()
     {
-        //InventorySystem.current.Add(referenceItem);
+        InventorySystem.current.Add(referenceItem); //singleton reference to inventory manager
         Destroy(gameObject);
     }
 }
