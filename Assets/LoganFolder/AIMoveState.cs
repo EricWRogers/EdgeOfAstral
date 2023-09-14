c35a176cbd4fba3a8fa52b8a01fb6cabbc831429
Logan106 <Loganhobson31@gmail.com>
2023-09-14 09:09:34 -0500

    Patrol attempt

--- Assets/LoganFolder/AIMoveState.cs
+++ Assets/LoganFolder/AIMoveState.cs
@@ -1,17 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Apple;
-using static UnityEngine.GraphicsBuffer;
 
 public class AIMoveState : MonoBehaviour, IEnemyState
 {
     private AIStateMachine stateMachine;
     private NavMeshAgent agent;
-    private GameObject target;
+    public GameObject target;
 
-  
+    public List<Transform> points = new List<Transform>();
+    private int destPoint = 0;
+
+    float radius = 0;
 
     public AIMoveState(AIStateMachine stateMachine)
     {
@@ -36,36 +37,84 @@ public class AIMoveState : MonoBehaviour, IEnemyState
 
         }
 
-        if(PathValid() == false)
+        if (PathValid(target.transform) == false)
         {
             Debug.Log("Cant reach em boss.");
 
-            return;
-           
+            GenereateWaypoints();
+
+            Patrol();
+
         }
     }
 
     public void Exit() //Last thing the state does before sending us wherever the user specified in update.
     {
-        
+
         Debug.Log("Exiting Move State");
-        
+
     }
 
     public void Patrol()
     {
-        if (PathValid() == true)
+        if (PathValid(target.transform) == true || points.Count == 0)
         {
+            agent.autoBraking = true;
+            points.Clear();
             return;
         }
 
+        agent.autoBraking = false;
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            agent.destination = points[destPoint].position;
+        }
+
+        destPoint = (destPoint + 1) % points.Count;
+
+
+    }
+    public bool PathValid(Transform _target)
+    {
+        return agent.CalculatePath(_target.position, agent.path);
+
+    }
+
+    public void GenereateWaypoints()
+    {
+        float i = 1;
+
+        if(FindPoints(i).Length <= 2)
+        {
+            Debug.Log("Iterator = " + i);
+            if(FindPoints(i++).Length >= 2)
+            {
+                foreach(RaycastHit hit in FindPoints(i))
+                {
+                    points.Add(hit.transform);
+                }
+                return;
+            }
+        }
+        else
+        {
+            Debug.Log("Idk man");
+        }
         
     }
-    public bool PathValid()
-    {
-        return agent.CalculatePath(target.transform.position, agent.path);
 
+    public RaycastHit[] FindPoints(float _radius)
+    {
+        radius = _radius;
+        Debug.Log("Thing = " + Physics.SphereCastAll(target.transform.position, _radius, transform.forward, 8));
+        return Physics.SphereCastAll(target.transform.position, _radius, transform.forward, 8);
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(target.transform.position, radius);
+    }
 
 }
