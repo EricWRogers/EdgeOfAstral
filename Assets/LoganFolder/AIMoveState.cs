628739f17baf02dfa71db05eea410f3bdac6926a
Logan Hobson <Loganhobson31@gmail.com>
2023-09-20 15:27:24 -0500

    Start from scratch I guess

--- Assets/LoganFolder/AIMoveState.cs
+++ Assets/LoganFolder/AIMoveState.cs
@@ -9,10 +9,7 @@ public class AIMoveState : MonoBehaviour, IEnemyState
     private NavMeshAgent agent;
     public GameObject target;
 
-    public List<Vector3> points = new List<Vector3>();
-    private int destPoint = 0;
-
-    public float radius = 1;
+    private SphereCollider detectCol;
 
     public AIMoveState(AIStateMachine stateMachine)
     {
@@ -25,6 +22,11 @@ public class AIMoveState : MonoBehaviour, IEnemyState
         //agent = GetComponentInParent<NavMeshAgent>();
         agent = FindAnyObjectByType<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player");
+
+        detectCol = target.AddComponent<SphereCollider>();
+
+        target.AddComponent<GetContacts>();
+
 
     }
 
@@ -41,24 +43,8 @@ public class AIMoveState : MonoBehaviour, IEnemyState
         {
             Debug.Log("Can't get there boss.");
 
-            if (GenerateWaypoints() && points.Count > 2) //IF this returns true, execute the patrol.
-            {
-                Debug.Log("Found a few points off the bat");
-                Patrol();
-            }
-            else //Increase the radius and try again if no waypoints were found.
-            {
-                Debug.Log("Looking for points 1");
-                
-                radius += 1;
-                GenerateWaypoints();
-
-            }
-
-            if (points.Count > 0)
-            {
-                Patrol();
-            }
+            GetPatrolPoints();
+            Patrol();
         }
        
     }
@@ -72,71 +58,25 @@ public class AIMoveState : MonoBehaviour, IEnemyState
 
     public void Patrol()
     {
-        if (PathValid(target.transform) == true || points.Count < 2)
-        {
-            Debug.Log("We aint patrollin.");
-            agent.autoBraking = true;
-            points.Clear();
-            return;
-        }
-        Debug.Log("We are patrolin.");
-        agent.autoBraking = false;
-
-        if (!agent.pathPending && agent.remainingDistance < 0.5f && points.Count != 0)
-        {
-            agent.destination = points[destPoint];
-        }
-        if (points.Count != 0)
-        {
-            destPoint = (destPoint + 1) % points.Count;
-        }
+        
 
     }
 
-    public bool GenerateWaypoints()
+    public void GetPatrolPoints()
     {
-        int i = 0;
-      
-        RaycastHit[] hits = FindPoints(radius);
-
-        if (hits.Length >= 2)
-        {
-            Debug.Log("We got points.");
-            foreach (RaycastHit hit in hits)
-            {
-                i++;
-                
-                Debug.Log("Hit " + i + " " + hit.point);
-                if (!points.Contains(hit.point))
-                {
-                    points.Add(hit.point);
-                    Debug.Log("Added Point" + hit.point);
-                    Debug.Log("Point count: " + points.Count);
-                }
-            }
-            return true;
-        }
-
-        Debug.Log("No point.");
-        return false;
-
+        target.GetComponent<GetContacts>();
+       
     }
 
-    public RaycastHit[] FindPoints(float _radius)
-    {
-        Debug.Log("Finding points");
-       // Debug.Log("Radius: " + _radius);
-        return Physics.SphereCastAll(target.transform.position, 10, Vector3.up, 0, 8)
-        .GroupBy(hit => hit.point) // Group hits by their point
-        .Select(group => group.First()) // Select the first hit from each group
-        .ToArray(); // Convert the result to an array
-    }
+    
+
+   
 
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(target.transform.position, 10);
+        Gizmos.DrawSphere(target.transform.position, radius);
      
     }
 
