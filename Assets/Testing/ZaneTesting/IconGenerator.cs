6e15a1863e1f341a4d2bcd8ead668412696a755e
Zane Camp <zmcamp1306@muleriders.saumag.edu>
2023-10-04 16:31:57 -0500

    Icon Generator Script Made

---
+++ Assets/Testing/ZaneTesting/IconGenerator.cs
@@ -0,0 +1,79 @@
+using OmnicatLabs.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public List<GameObject> sceneObjects;
+public List<InventoryItemData> dataObjects;
+
+void Awake()
+{
+    camera = GetComponent<Camera>();
+}
+
+[ContextMenu("Screenshot")]
+void ProcessScreenshot()
+{
+    StartCoroutine(ScreenShot());
+}
+
+public class IconGenerator : MonoBehaviour
+{
+    void TakeScreenshot(string fullPath)
+    {
+        if (GetComponent<Camera>() == null)
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        RenderTexture rt = new RenderTexture(256, 256, 24);
+        GetComponent<Camera>().targetTexture = rt;
+        Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        GetComponent<Camera>().Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+        GetComponent<Camera>().targetTexture = null;
+        RenderTexture.active = null;
+
+        if (Application.isEditor)
+        {
+            DestroyImmediate(rt);
+        }
+        else
+        {
+            Destroy(rt);
+        }
+        byte[] bytes = screenShot.EncodeToPNG();
+        InventorySystem.IO.File.WriteAllBytes(fullPath, bytes);
+#if UNITY_EDITOR
+    AssetDatabase.Refresh();
+#endif
+    }
+
+    private IEnumerator Screenshot()
+    {
+        for (int i = 0; i < screenObjects.Count; i++)
+        {
+            GameObject obj = sceneObjects[i];
+            InventoryItemData data = dataObjects[i];
+
+            obj.gameObject.SetActive(true);
+
+            yield return null;
+
+            TakeScreenshot($"{Application.dataPath}/{pathFolder}/{data.id}_Icon.png");
+
+            yield return null;
+            obj.gameObject.SetActive(false);
+
+            Sprite s = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/{pathFolder}/{data.id}_Icon.png");
+            if (s != null )
+            {
+                data.icon = s;
+                EditorUtility.SetDirty( data );
+            }
+            yield return null;
+        }
+    }
+}
+
