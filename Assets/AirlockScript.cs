665509c499659678a995d6a05dff380886c17587
Brandon Lee <b.lee42000@gmail.com>
2023-10-09 17:36:33 -0500

    Airlock checkpoint made and working

---
+++ Assets/AirlockScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirlockScript : MonoBehaviour
+{
+    public GameObject NextArea;
+    public GameObject PrevioiusArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
