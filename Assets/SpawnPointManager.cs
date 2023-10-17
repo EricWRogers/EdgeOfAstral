05335d049e0d17550c94b67b8c1cf89d645a1fdc
Darrel Odom <ddodom17@gmail.com>
2023-10-16 19:50:12 -0500

    Fixes part 2
    Everything should be working now. I marked in code an error I'm having where when I expose my lists to the editor with a serializefield I get an 'objectDisposedException' 9 times out of 10. It doesn't break anything that I can tell but it does it all the same. I also couldn't pull your latest branch into mine due to merge conflicts? Not sure why I would be getting those, but I'll make a pull request for what I have.

--- Assets/SpawnPointManager.cs
+++ Assets/SpawnPointManager.cs
@@ -11,16 +11,25 @@ public class SpawnPointManager : MonoBehaviour
     public int numberOfItemsToSpawn;
     public float rotationSpeed = 45f;
 
+     
+    // EXPOSING THE LISTS IN THE EDITOR CAUSES 'ObjectDisposedException' ERRORS. MAYBE I'M
+    // NOT CLEARING THE LIST RIGHT. IDK.
+    
     [Header("Spawn Points Variables")]
-    public int totalSpawnPoints;
+    //[SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+    //[SerializeField] private List<GameObject> spawnedItems = new List<GameObject>();
+    [SerializeField] public int totalSpawnPoints;
     [SerializeField] private int remainingStaminaItems;
 
-    private List<Transform> spawnPoints = new List<Transform>();
     private List<GameObject> spawnedItems = new List<GameObject>();
+    private List<Transform> spawnPoints = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
     {
+        //SaveManager.Instance.onReset.AddListener(SpawnPointManager);
+        //spawnPoints.Clear();
+
         foreach (Transform child in transform)
         {
             spawnPoints.Add(child);
@@ -34,16 +43,25 @@ public class SpawnPointManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {       
+    {
+        List<GameObject> itemsToRemove = new List<GameObject>();
+
         foreach (GameObject item in spawnedItems)
         {
-            // Rotates the spawned items
-            item.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            if (item == null)
+            {
+                itemsToRemove.Add(item);
+            }
+            else
+            {
+                // Rotates the spawned items
+                item.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            }
         }
 
+        spawnedItems.RemoveAll(item => itemsToRemove.Contains(item));
         GameObject[] staminaItems = GameObject.FindGameObjectsWithTag("StaminaItem");
         remainingStaminaItems = staminaItems.Length;
-        //Quaternion rotation = Quaternion.Euler(0f, 90f, 0f);
     }
 
     void SpawnStaminaItems()
@@ -51,7 +69,7 @@ public class SpawnPointManager : MonoBehaviour
         // Checks to makes sure that the number of items spawned does not exceed the number of available spawn points
         if (numberOfItemsToSpawn > totalSpawnPoints)
         {
-            Debug.LogError("Error: The number of spawned items cannot exceed the total number of spawn points avaliable.");
+            Debug.LogError("Error: The number of items to spawn cannot exceed the total number of spawn points avaliable.");
             
             numberOfItemsToSpawn = totalSpawnPoints;
         }
@@ -76,11 +94,6 @@ public class SpawnPointManager : MonoBehaviour
             itemsSpawned++;
             spawnedItems.Add(staminaItem);
         }
-    }
-
-    public int GetRemainingItemsInSceneCount()
-    {
-        return remainingStaminaItems;
     }
 
     //SaveManager.Instance.onReset.AddListener(SpawnPointManager);
