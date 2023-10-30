e76b855789e3be2cc91d172456f3de829865a2ed
RealDragonHours <ben.lawrence0324@gmail.com>
2023-10-30 01:04:25 -0500

    Fixed most issues from this week before merging other people's work in

--- Assets/Testing/DuelTesting/Launch.cs
+++ Assets/Testing/DuelTesting/Launch.cs
@@ -11,12 +11,19 @@ public class Launch : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.TryGetComponentInParentAndChildren(out Rigidbody rb);
-
-        if (other.CompareTag(playerTag) && rb.velocity.y < 0f)
+        if (UpgradeManager.ownedUpgrades.Contains(UpgradeIds.MagBoots))
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-            rb.AddForce(new Vector3(0f, launchForce, 0f), ForceMode.Impulse);
+            other.transform.TryGetComponentInParentAndChildren(out Rigidbody rb);
+
+            if (other.CompareTag(playerTag) && rb.velocity.y < 0f)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+                rb.AddForce(new Vector3(0f, launchForce, 0f), ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            GetComponent<Dialogue>().TriggerDialogue();
         }
     }
 
