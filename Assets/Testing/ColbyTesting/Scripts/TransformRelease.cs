42d336b2a705781f74b154512a972db9b6f6a5bb
Colby <colbykade626@gmail.com>
2023-11-21 14:33:23 -0600

    Prevent Overlap is now consistent with all colliders

--- Assets/Testing/ColbyTesting/Scripts/TransformRelease.cs
+++ Assets/Testing/ColbyTesting/Scripts/TransformRelease.cs
@@ -69,10 +69,10 @@ public class ObjectPositionTracker
             }
         }
     }
-
-    private static bool CheckForOverlaps(GameObject gameObject)
+    private static bool CheckForOverlaps(GameObject myobject)
     {
-        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        MeshRenderer meshRenderer = myobject.GetComponent<MeshRenderer>();
+        Collider myCollider = myobject.GetComponent<Collider>();
 
         if (meshRenderer == null)
         {
@@ -81,100 +81,24 @@ public class ObjectPositionTracker
 
         Bounds boundsToCheck = meshRenderer.bounds;
 
-        // Loop over other objects
+        //Find all colliders around the object you are moving
         foreach (Collider collider in Physics.OverlapBox(boundsToCheck.center, boundsToCheck.extents))
         {
             GameObject otherObject = collider.gameObject;
 
-            if (otherObject != gameObject)
+            if (otherObject != myobject)
             {
-                MeshCollider otherMeshCollider = otherObject.GetComponent<MeshCollider>() ?? otherObject.AddComponent<MeshCollider>();
-                Ray ray = new(boundsToCheck.center, (otherObject.transform.position - boundsToCheck.center).normalized);
-                if (RayIntersectsMesh(ray, otherMeshCollider, out _))
-                {
-                    return true; // Overlap detected
-                }
+                return Physics.ComputePenetration(
+                myCollider,
+                myCollider.transform.position,
+                myCollider.transform.rotation,
+                collider,
+                collider.transform.position,
+                collider.transform.rotation,
+                out Vector3 _, out float _);
             }
         }
 
         return false; // No overlap detected
-    }
-
-    private static bool RayIntersectsMesh(Ray ray, MeshCollider meshCollider, out RaycastHit hit)
-    {
-        hit = new RaycastHit();
-
-        if (meshCollider == null)
-        {
-            return false;
-        }
-
-        Mesh mesh = meshCollider.sharedMesh;
-
-        if (mesh == null)
-        {
-            return false;
-        }
-
-        int[] triangles = mesh.triangles;
-        Vector3[] vertices = mesh.vertices;
-
-        for (int i = 0; i < triangles.Length; i += 3)
-        {
-            Vector3 vertex0 = vertices[triangles[i]];
-            Vector3 vertex1 = vertices[triangles[i + 1]];
-            Vector3 vertex2 = vertices[triangles[i + 2]];
-
-            if (RayIntersectsTriangle(ray, vertex0, vertex1, vertex2, out float distance))
-            {
-                if (hit.collider == null || distance < hit.distance)
-                {
-                    if (Physics.Raycast(ray, out RaycastHit meshHit, distance))
-                    {
-                        hit = meshHit;
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private static bool RayIntersectsTriangle(Ray ray, Vector3 vertex0, Vector3 vertex1, Vector3 vertex2, out float distance)
-    {
-        distance = 0f;
-
-        Vector3 edge1 = vertex1 - vertex0;
-        Vector3 edge2 = vertex2 - vertex0;
-
-        Vector3 h = Vector3.Cross(ray.direction, edge2);
-        float a = Vector3.Dot(edge1, h);
-
-        if (a > -float.Epsilon && a < float.Epsilon)
-        {
-            return false; // The ray is parallel to the triangle
-        }
-
-        float f = 1.0f / a;
-        Vector3 s = ray.origin - vertex0;
-        float u = f * Vector3.Dot(s, h);
-
-        if (u < 0.0f || u > 1.0f)
-        {
-            return false;
-        }
-
-        Vector3 q = Vector3.Cross(s, edge1);
-        float v = f * Vector3.Dot(ray.direction, q);
-
-        if (v < 0.0f || u + v > 1.0f)
-        {
-            return false;
-        }
-
-        distance = f * Vector3.Dot(edge2, q);
-
-        return distance > float.Epsilon;
     }
 }
