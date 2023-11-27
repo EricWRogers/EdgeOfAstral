ed02e42fb81ffb7f15806b59157312e208583e2a
Brandon Lee <b.lee42000@gmail.com>
2023-11-26 23:45:26 -0600

    Made a system capable of saving and loading data between play sessions.

---
+++ Assets/Testing/BrandonTesting/GameData.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+
+    public int GameInteger { get; set; }
+    public string GameString { get; set; }
+    public bool GameBool { get; set; }
+    public float GameSpawn { get; set; }
+    /*[SerializeField]
+    private Text textInteger;
+    [SerializeField]
+    private Text textString;
+
+    public void GenerateNewData()
+    {
+        GameInteger = Random.Range(1, 1000);
+        GameString = System.Convert.ToBase64String(System.BitConverter.GetBytes(GameInteger));
+        ShowData();
+    }
+
+    public void ShowData()
+    {
+        textInteger.text = GameInteger.ToString();
+        textString.text = GameString;
+    }*/
+}
---
+++ Assets/Testing/BrandonTesting/Save.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Save
+{
+    /*public int SavedInteger;
+    public string SavedString;
+    public bool SavedBool;*/
+
+    public Transform SavedSpawn;
+}
