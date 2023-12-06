8bbb3bb4064af9115851659ba0cbf5693d085699
Logan Hobson <Loganhobson31@gmail.com>
2023-12-06 05:12:27 -0600

    Enviorment Assets

--- Assets/RubbleSound.cs
+++ Assets/RubbleSound.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using OmnicatLabs.Audio;
+using UnityEditor.Timeline.Actions;
 
 public class RubbleSound : MonoBehaviour
 {
-
+    Animator anim;
+    AnimationClip clip;
     public void Sound()
     {
+       Timeline
+
         AudioManager.Instance.Play("RubbleCrash");
     }
 }
