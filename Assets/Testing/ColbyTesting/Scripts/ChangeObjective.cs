6cceeb7c5d9f8ae64d01cb54ba41f28b341fd845
Logan Hobson <Loganhobson31@gmail.com>
2023-12-05 18:58:29 -0600

    Merge/Navmesh adjustments

--- Assets/Testing/ColbyTesting/Scripts/ChangeObjective.cs
+++ Assets/Testing/ColbyTesting/Scripts/ChangeObjective.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class ChangeObjective : MonoBehaviour
 {
+    public int priority = 0;
     [TextArea]
     public string newObjective;
     public void Change()
     {
-        ObjectiveText.instance.SetCurrObjective(newObjective);
+        if (priority > ObjectiveText.instance.currObjectivePriority)
+        {
+            ObjectiveText.instance.SetCurrObjective(newObjective);
+            ObjectiveText.instance.SetCurrObjectivePriority(priority);
+        }
     }
 }
