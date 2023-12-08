49ee8b4cfe4b448e15f83ebad0ef73982b0f17fd
RealDragonHours <ben.lawrence0324@gmail.com>
2023-12-08 08:48:34 -0600

    yep mhm

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,41 +1,120 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 
 public class SettingsMenu : MonoBehaviour
 {
     public UIStateMachineController controller;
     public Button backButton;
+    public Button audioReturnButton;
+    public Button displayOptionsButton;
     public CanvasGroup mainMenu;
+    public CanvasGroup displaySettingsMenu;
+    public CanvasGroup soundSettingsMenu;
+    public CanvasGroup pauseMenu;
+
     private void Start()
     {
         backButton.onClick.AddListener(ExitToMainMenu);
+        audioReturnButton.onClick.AddListener(OpenAudioSettingsMenu);
+        displayOptionsButton.onClick.AddListener(OpenDisplayMenu);
     }
 
     private void PostPlay()
     {
         backButton.onClick.RemoveListener(ExitToMainMenu);
         backButton.onClick.AddListener(Exit);
+
+        //audioReturnButton.onClick.RemoveListener(OpenAudioSettingsMenu);
+        //audioReturnButton.onClick.AddListener(OpenDisplayMenu);
+
+        //displayOptionsButton.onClick.RemoveListener(OpenDisplayMenu);
+        //displayOptionsButton.onClick.AddListener(OpenAudioSettingsMenu);
     }
 
-    public void ExitToMainMenu()
+    public void OpenDisplayMenu()
     {
-        GetComponent<CanvasGroup>().alpha = 0f;
-        //controller.ChangeState(controller.nullState);
-        GetComponent<CanvasGroup>().interactable = false;
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        backButton.onClick.RemoveAllListeners();
+        audioReturnButton.onClick.RemoveAllListeners();
+        audioReturnButton.onClick.AddListener(OpenAudioSettingsMenu);
+        //mainMenu.alpha = 0f;
+        displaySettingsMenu.alpha = 1f;
+        soundSettingsMenu.alpha = 0f;
+
+        mainMenu.interactable = false;
+        displaySettingsMenu.interactable = true;
+        soundSettingsMenu.interactable = false;
+
+        mainMenu.blocksRaycasts = false;
+        displaySettingsMenu.blocksRaycasts = true;
+        soundSettingsMenu.blocksRaycasts = false;
+
+        GetComponentInChildren<Slider>().interactable = true;
+
+        Debug.Log("Opening Graphics Display Menu");
+    }
+
+    public void OpenAudioSettingsMenu()
+    {
+        backButton.onClick.RemoveAllListeners();
+        audioReturnButton.onClick.RemoveAllListeners();
+        backButton.onClick.AddListener(Exit);
+        //mainMenu.alpha = 0f;
+        displaySettingsMenu.alpha = 0f;
+        soundSettingsMenu.alpha = 1f;
+
+        mainMenu.interactable = false;
+        displaySettingsMenu.interactable = false;
+        soundSettingsMenu.interactable = true;
+
+        mainMenu.blocksRaycasts = false;
+        displaySettingsMenu.blocksRaycasts = false;
+        soundSettingsMenu.blocksRaycasts = true;
+
+        GetComponentInChildren<Slider>().interactable = true;
+
+        Debug.Log("Opening Audio Settings Menu");
+    }
+
+        public void ExitToMainMenu()
+    {
+        mainMenu.alpha = 1f;
+        displaySettingsMenu.alpha = 0f;
+        soundSettingsMenu.alpha = 0f;
+        soundSettingsMenu.interactable = false;
+        soundSettingsMenu.blocksRaycasts = false;
+
         mainMenu.interactable = true;
-        GetComponentInChildren<Slider>().interactable = false;
+        mainMenu.blocksRaycasts = true;
+
+        displaySettingsMenu.interactable = false;
+        soundSettingsMenu.interactable = false;
+
+        //GetComponentInChildren<Slider>().interactable = false;
+
     }
 
     public void Exit()
     {
-        GetComponent<CanvasGroup>().alpha = 0f;
-        controller.ChangeState<HUDPauseState>();
-        GetComponent<CanvasGroup>().interactable = false;
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
-        GetComponentInChildren<Slider>().interactable = false;
+        mainMenu.alpha = 0f;
+        displaySettingsMenu.alpha = 0f;
+        soundSettingsMenu.alpha = 0f;
+
+        mainMenu.interactable = false;
+        displaySettingsMenu.interactable = false;
+        soundSettingsMenu.interactable = false;
+
+        mainMenu.blocksRaycasts = false;
+        displaySettingsMenu.blocksRaycasts = false;
+        soundSettingsMenu.blocksRaycasts = false;
+
+        //controller.ChangeState(controller.nullState);
+        pauseMenu.alpha = 1f;
+        pauseMenu.interactable = true;
+        pauseMenu.blocksRaycasts = true;
+
+       // GetComponentInChildren<Slider>().interactable = false;
+
     }
+    
 }
